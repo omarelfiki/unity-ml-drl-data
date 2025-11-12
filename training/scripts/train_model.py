0abbb411f567c66e41b165a399e1c072818b0908
Omar Elfiki <me@omarelfiki.com>
2025-11-12 15:20:38 +0100

    seperating threshold generation from data summarization and validation. Adding --seed flag to training signature to allow seed editing

--- training/scripts/train_model.py
+++ training/scripts/train_model.py
@@ -144,12 +144,14 @@ def parse_arguments_and_load_config():
     parser.add_argument("--num-steps", type=int, default=N_STEPS, help="Number of steps to monitor")
     parser.add_argument("--headless", dest="env_path", required=False, help="Path to the build so it can train headless (no graphics)")
     parser.add_argument("--ac", action="store_true", help="Activate auto-commit")
+    parser.add_argument("--seed", type=int, help="(Optional) Seed used for data replication")
     args = parser.parse_args()
 
     config_file = args.config
     run_id = args.run_id
     num_steps = args.num_steps
     env_path = args.env_path
+    seed = args.seed
     ac = args.ac
     if ac:
         print(f"[INFO]: Auto-commit activated for run: '{run_id}'")
@@ -163,12 +165,13 @@ def parse_arguments_and_load_config():
     except Exception as e:
         print(f"[ERROR] Could not read config file '{config_file}': {e}")
 
-    return config_file, run_id, num_steps, config_data, ac, env_path
+    return config_file, run_id, num_steps, config_data, ac, env_path, seed
 
-def run_training_process(config_file, run_id, env_path):
+def run_training_process(config_file, run_id, env_path, seed):
     print(f"\n Starting ML-Agents training")
     print(f"   Config file: {config_file}")
     print(f"   Run ID:      {run_id}")
+    print(f"   Seed:        {seed}")
     
     if env_path:
         print(f"   Env build:   {env_path}")
@@ -188,6 +191,8 @@ def run_training_process(config_file, run_id, env_path):
         cmd = ["mlagents-learn", config_file, "--run-id", run_id, "--force", "--no-graphics"]
         if env_path:
             cmd.extend(["--env", env_path])
+        if seed:
+            cmd.extend(["--seed", str(seed)])
         process = subprocess.Popen(
             cmd,
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
@@ -533,8 +538,8 @@ def auto_commit_results(commit_message="Auto-update: new training results"):
         print(f"[ERROR] Unexpected error: {e}")
 
 def main():
-    config_file, run_id, num_steps, config_data, ac, env_path = parse_arguments_and_load_config()
-    total_time, mean_cpu, mean_ram = run_training_process(config_file, run_id, env_path)
+    config_file, run_id, num_steps, config_data, ac, env_path, seed = parse_arguments_and_load_config()
+    total_time, mean_cpu, mean_ram = run_training_process(config_file, run_id, env_path, seed)
     combined_data, behavior_name, environment, total_time, log_dir = analyze_training_results(run_id, config_file, num_steps, config_data, total_time, mean_cpu, mean_ram)
     analyze_thresholds(run_id, behavior_name, environment, total_time, log_dir, combined_data)
     save_and_display_results(combined_data)
